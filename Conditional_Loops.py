c19f1155dbd3d4060a091276d4b73797a771591c
AtharvaChennur <47922007+AtharvaChennur@users.noreply.github.com>
2020-04-29 17:54:04 +0530

    Create Conditional_Loops.py

---
+++ Conditional_Loops.py
@@ -0,0 +1,9 @@
+if(c==12):
+    if (c < 2):
+        print("It is addition")
+    elif(d<2):
+        print("Subtarction is useful")
+    elif(d<5):
+        print("It is not division")
+    else:
+        print("It is nit multiplication")
