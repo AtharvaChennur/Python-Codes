2829447dff3f49569b25cf18173c4d3a097e9538
AtharvaChennur <47922007+AtharvaChennur@users.noreply.github.com>
2020-04-29 17:43:22 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a,b = input("enter values for a and b").split()
+a= int(a)
+b = int(b)
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+print("Value of addition is {} and subtraction is {} and multiplication is {} and division is {}".format(c,d,e,f));
